317c7d2ce87e5293fefbf33ae25892e7ce7e2965
Henrique Silva <henrique.silva@lnls.br>
2018-08-20 14:34:46 -0300

    read_loop.py: improve error handling

--- read_loop.py
+++ read_loop.py
@@ -6,6 +6,7 @@ import time
 from time import sleep
 from TH2E import TH2E
 import argparse
+import socket
 
 parser = argparse.ArgumentParser()
 parser.add_argument('output', help='output file where the readings will be written')
@@ -14,7 +15,6 @@ parser.add_argument('-i','--ip', help='set sensor ip', default='10.2.117.254')
 parser.add_argument('-u','--utc', help='Use UTC time as reference', action='store_true')
 args = parser.parse_args()
 
-sensor = TH2E(args.ip)
 abspath = os.path.abspath(args.output)
 if not os.path.exists(os.path.dirname(abspath)):
     try:
@@ -38,18 +38,33 @@ print('\nThe following sensors are being monitored, press ctrl-c to stop...')
 print('Time [{}]\t\tTemperature\tHumidity\tDew Point\n'.format(time_fmt)),
 
 while True:
-    resp = sensor.read_all()
-    if len(read) == 3:
-        temp, hum, dew = resp
-        print('\r'+str(last_read_time)+'\t'+str(temp)+'\t'+str(hum)+'\t'+str(dew)),
-        with open(abspath+'/th2e_readings.txt', 'a') as f:
-            f.write(str(last_read_time)+'\t'+str(temp)+'\t'+str(hum)+'\t'+str(dew)+'\n')
-        sys.stdout.flush()
-        try:
-            while time.time() - last_read_time < args.delay:
-                sleep(1)
-        except KeyboardInterrupt:
-            f.close()
-            break
+    try:
+        sensor = TH2E(args.ip)
+        while True:
+            try:
+                if args.utc:
+                    time_data = time.gmtime(time.time())
+                else:
+                    time_data = time.localtime(time.time())
 
-        last_read_time = time.time()
+                resp = sensor.read_all()
+                if len(resp) == 3:
+                    temp, hum, dew = resp
+                    data_str = time.strftime('%Y/%m/%d %H:%M:%S', time_data)+'\t'+str(temp)+'\t\t'+str(hum)+'\t\t'+str(dew)+'\n'
+                    print(data_str),
+                    f.write(data_str)
+                    sys.stdout.flush()
+                    sleep(args.delay)
+            except ValueError as e:
+                print(e)
+                pass
+            except KeyboardInterrupt:
+                raise
+    except socket.error:
+        #Wait some time before trying to connect again
+        sleep(1)
+        pass
+    except KeyboardInterrupt:
+        f.close()
+        sensor.close()
+        exit()
